9904cc832660f1319b0883a854e85a0bf1cd9c04
AartiPardeshi23 <137265902+AartiPardeshi23@users.noreply.github.com>
2025-11-20 11:30:27 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,8 +11,8 @@ terraform {
 provider "azurerm" {
   features {}
 
+  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
