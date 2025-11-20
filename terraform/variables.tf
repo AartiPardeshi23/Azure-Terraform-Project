4add9386115203eca07abaf49cd82185a2eb5195
AartiPardeshi23 <137265902+AartiPardeshi23@users.noreply.github.com>
2025-11-20 17:06:26 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,30 +1,10 @@
-variable "subscription_id" {
-  type        = string
-  description = "Azure Subscription ID"
-}
-
-variable "client_id" {
-  type        = string
-  description = "Azure AD App Client ID"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "Azure AD App Client Secret"
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "Azure Tenant ID"
-}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
 
 variable "admin_username" {
-  type        = string
-  description = "VM admin username"
+  default = "azureuser"
 }
 
-variable "ssh_public_key" {
-  type        = string
-  description = "SSH public key for VM login"
-}
+variable "ssh_public_key" {}
